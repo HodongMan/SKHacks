08a5fc59495a2ff297bae8ac7da2b96d56232e28
hodongman <arshavin3@naver.com>
2017-08-23 17:46:10 +0900

    model update extends many to many

--- sk/models.py
+++ sk/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+
+    MALE='M'
+    FEMALE='F'
+    GENDER_CHOICES= (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+
+    Name = models.CharField(max_length=200)
+    Major = models.CharField(max_length=200, blank=True, default='')
+    Gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    Birth = models.DateField()
+    Age = models.TextField()
+    Description = models.IntegerField()
+    Created = models.DateTimeField(auto_now_add=True)
+
+class Professor(models.Model):
+
+    Name = models.CharField(max_length=200)
+    Major = models.CharField(max_length=200)
+    Created = models.DateTimeField(auto_now_add=True)
+
+class Lecture(models.Model):
+
+    Professor = models.ForeignKey(
+        Professor,
+        related_name='Lectures',
+        on_delete=models.CASCADE
+    )
+    Name = models.CharField(max_length=200)
+    Description = models.TextField()
+    Max_chapter = models.IntegerField()
+    Created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('Created',)
+
+class Chapter(models.Model):
+
+    Lecture = models.ForeignKey(
+        Lecture,
+        related_name='Chapters',
+        on_delete=models.CASCADE
+    )
+
+    Current_chapter = models.IntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+class Attendence(models.Model):
+
+    Chapter = models.ForeignKey(
+        Chapter,
+        related_name='Chapters',
+        on_delete=models.CASCADE
+    )
+    Student = models.ForeignKey(
+        Student,
+        related_name='Students',
+        on_delete=models.CASCADE
+    )
+    Lecture = models.ForeignKey(
+        Lecture,
+        related_name='Lectuers',
+        on_delete=models.CASCADE
+    )
--- skapi/settings.py
+++ skapi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'skapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sk',
+        'USER' : 'root',
+        'PASSWORD' : '1234',
+        'HOST' : '127.0.01',
+        'PORT' : '3306',
     }
 }
 
